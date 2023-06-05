45427fa3da0f7ac068bcf84305ceaea92e3e511a
biplab990 <biplabuddin990@gmail.com>
2023-06-05 07:35:37 +0600

    up

--- include/footer.php
+++ include/footer.php
@@ -5,7 +5,7 @@
   <!-- /.control-sidebar -->
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2022 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; Biplab Uddin.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 3.2.0
